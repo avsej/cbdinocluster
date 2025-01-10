81adb83fb0b903fffc4e96bfc9a87407112b12f9
Sergey Avseyev <sergey.avseyev@gmail.com>
2025-01-10 12:59:52 -0800

    Add support for toy build images

--- utils/versionident/versionident.go
+++ utils/versionident/versionident.go
@@ -13,12 +13,14 @@ type Version struct {
 	BuildNo          int
 	CommunityEdition bool
 	Serverless       bool
+	Owner            string
 }
 
 func Identify(ctx context.Context, userInput string) (*Version, error) {
 	editionPart := "enterprise"
 	versionPart := ""
 	buildNoPart := "0"
+	ownerPart := ""
 
 	versionParts := strings.Split(userInput, "-")
 
@@ -43,6 +45,11 @@ func Identify(ctx context.Context, userInput string) (*Version, error) {
 		editionPart = versionParts[0]
 		versionPart = versionParts[1]
 		buildNoPart = versionParts[2]
+	} else if len(versionParts) == 4 {
+		ownerPart = versionParts[0]
+		editionPart = versionParts[1]
+		versionPart = versionParts[2]
+		buildNoPart = versionParts[3]
 	}
 
 	communityEdition := false
@@ -68,5 +75,6 @@ func Identify(ctx context.Context, userInput string) (*Version, error) {
 		BuildNo:          int(buildNo),
 		CommunityEdition: communityEdition,
 		Serverless:       serverless,
+		Owner:            ownerPart,
 	}, nil
 }
